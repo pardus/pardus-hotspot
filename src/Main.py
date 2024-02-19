2c818c350a6403fe15de91354d07f37ea1668ccc
Emel Öztürk <xmlozturk@gmail.com>
2024-02-19 16:34:22 +0300

    Write main without checking __name__

--- src/Main.py
+++ src/Main.py
@@ -2,6 +2,7 @@
 
 import sys
 import gi
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import GLib, Gio, Gtk
 
@@ -9,13 +10,27 @@ from MainWindow import MainWindow
 
 class Application(Gtk.Application):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, application_id="tr.org.pardus.pardus-hotspot", **kwargs)
+        super().__init__(*args, application_id="tr.org.pardus.pardus-hotspot",
+                         flags=Gio.ApplicationFlags.FLAGS_NONE, **kwargs
+        )
         self.window = None
     
     def do_activate(self):
-        self.window = MainWindow(self)
+        # We only allow a single window and raise any existing ones
+        if not self.window:
+            self.window = MainWindow(self)
+        else:
+            self.window.present()
 
 
-if __name__ == "__main__":
-    app = Application()
-    app.run(sys.argv)
+
+    def do_command_line(self, command_line):
+        options = command_line.get_options_dict()
+        options = options.end().unpack()
+        self.args = options
+        self.activate()
+        return 0
+
+# Application instance is created and run here directly without checking __name__
+app = Application()
+app.run(sys.argv)
